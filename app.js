726afbf4356036f014c9cb3226f7f16005d0b0eb
Fabiano Fuzetto <fabianofuzetto@gmail.com>
2024-06-10 01:05:57 -0300

    ImplementacaoRotas

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
-const expreess = require("express");
-const os = require("os");
-const app = expreess();
+const express = require('express');
+const app = express();
 
-app.get("/", (request, response) => {
-    return response
-    .status(200)
-    .json({
-        message: "Hello World"
-    });
+app.get('/liveness', (request, response) => {
+  return response.status(200).json({
+    message: 'Trabalho dificil da pega!',
+  });
 });
 
-app.get("liveness", (request, response) => {
-    return response
-    .status(200)
-    .json({
-        message: "Meu App está Vivo",
-        path: process.cwd(),
-        gid: process.getgid(),
-        uid: process.getuid(),
-        date: new Date().getTime(),
-    });
+app.get('/readiness', (req, res) => {
+  return res.status(200).json({
+    message: 'Quero nota 10!'
+  });
 });
 
-app.get("/readiness", (request, response) => {
-    return response
-    .status(200)
-    .json({
-        message: "Meu App esta pronto",
-        platform: os.platform(),
-        freemem: os.freemem(),
-        homedir: os.homedir(),
-        date: new Date().getTime(),
-    });
-});
-
-
-
-
-module.exports = app;ede
+module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
-const http = require("http");
-const app = require("./app");
+const express = require('express');
+const mysql = require('mysql2');
+const appRoutes = require('./app');
 
+const app = express();
 const port = 3000;
-const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log("Server running on port" + port);
+const dbConfig = {
+  host: 'db', 
+  user: 'user',
+  password: '123456',
+  database: 'db_atividade4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+};
+
+const pool = mysql.createPool(dbConfig);
+
+
+app.get('/consulta-dados', (request, response) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      return response.status(500).json({ error: err.message });
+    }
+
+    connection.query('SELECT * FROM Clientes', (error, results) => {
+      connection.release(); 
+
+      if (error) {
+        return response.status(500).json({ error: error.message });
+      }
+      response.json(results);
+    });
+  });
+});
+
+app.use('/', appRoutes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
